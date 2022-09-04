69a965d8c267e46aa6ac837f6028b8e69afa3e4a
Valeri Neuskaya <valerineuskaya@yandex.by>
2022-09-04 18:10:17 +0200

    filters

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_filter',
+    'django_filters',
     'rest_framework',
     'users',
     'reviews',
